325dc1bb95710fcc3b849a5fb344abec652e9799
Andreina Cota Vidaurre <andycotvy@gmail.com>
2022-12-06 16:10:38 -0300

    Update startup_multilang.sh

--- startup_multilang.sh
+++ startup_multilang.sh
@@ -32,7 +32,7 @@ echo "downloading SU time"
 pip install sutime
 mkdir sutime
 cd sutime
-wget wget https://www.dropbox.com/s/smayhjqgzlhxpwc/jars.zip?dl=0
+wget https://www.dropbox.com/s/smayhjqgzlhxpwc/jars.zip?dl=0
 unzip jars.zip?dl=0
 rm jars.zip?dl=0
 cd ../..
@@ -57,15 +57,17 @@ bzcat latest-truthy.nt.bz2 | grep -E "schema.org/name|skos/core#altLabel" | awk 
 
 # DBpedia -Wikidata Sameas
 echo "download DBpedia-Wikidata Same As links dump: wikidatawiki-20150330-sameas-all-wikis.ttl.bz2"
-wget http://wikidata.dbpedia.org/downloads/20150330/wikidatawiki-20150330-sameas-all-wikis.ttl.bz2
+# wget http://wikidata.dbpedia.org/downloads/20150330/wikidatawiki-20150330-sameas-all-wikis.ttl.bz2
+wget http://downloads.dbpedia.org/3.9/en/iri_same_as_uri_en.ttl.bz2
 echo "creating a csv dictionary out of english dbpedia uris to wikidata same as links"
-bzcat wikidatawiki-20150330-sameas-all-wikis.ttl.bz2 | grep "http://dbpedia.org" | awk '{ print $3 " " $1}'  | sed -e 's/wikidata\.dbpedia\.org\/resource/www.wikidata\.org\/entity/'  | sed -e 's/ /\t/' | sed -E 's/[<>\"]//g' > dbpedia-wikidata-sameas-dict.csv
+# bzcat wikidatawiki-20150330-sameas-all-wikis.ttl.bz2 | grep "http://dbpedia.org" | awk '{ print $3 " " $1}'  | sed -e 's/wikidata\.dbpedia\.org\/resource/www.wikidata\.org\/entity/'  | sed -e 's/ /\t/' | sed -E 's/[<>\"]//g' > dbpedia-wikidata-sameas-dict.csv
+bzcat iri_same_as_uri_en.ttl.bz2 | grep "http://dbpedia.org" | awk '{ print $3 " " $1}'  | sed -e 's/wikidata\.dbpedia\.org\/resource/www.wikidata\.org\/entity/'  | sed -e 's/ /\t/' | sed -E 's/[<>\"]//g' > dbpedia-wikidata-sameas-dict.csv
 
-# Wikidata properties labels
-echo "download wikidata properties labels"
-wget http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/wikidata-properties.nt.gz
-zcat wikidata-properties.nt | grep "http://www.w3.org/2000/01/rdf-schema#label\|http://www.w3.org/2004/02/skos/core#altLabel" | grep "@en " | sed 's/^<//' | sed 's/.\{5\}$//' | sed 's/> </\t/g' | sed 's/> "/\t"/g' | sed 's/entity\/P/prop\/direct\/P/g'  > wikidata-properties.csv
-rm wikidata-properties.nt.gz
+# # Wikidata properties labels
+# echo "download wikidata properties labels"
+# wget http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/wikidata-properties.nt.gz
+# zcat wikidata-properties.nt | grep "http://www.w3.org/2000/01/rdf-schema#label\|http://www.w3.org/2004/02/skos/core#altLabel" | grep "@en " | sed 's/^<//' | sed 's/.\{5\}$//' | sed 's/> </\t/g' | sed 's/> "/\t"/g' | sed 's/entity\/P/prop\/direct\/P/g'  > wikidata-properties.csv
+# rm wikidata-properties.nt.gz
 
 cd ../..
 
@@ -98,9 +100,11 @@ elif [ $1 == "es" ]; then
 #     wget http://downloads.dbpedia.org/2016-10/core-i18n/es/long_abstracts_wkd_uris_es.ttl.bz2
     wget http://downloads.dbpedia.org/3.9/es/long_abstracts_en_uris_es.ttl.bz2
     echo "unzipping .."
-    bzip2 -dk long_abstracts_wkd_uris_es.ttl.bz2  #unzip keep original
+#     bzip2 -dk long_abstracts_wkd_uris_es.ttl.bz2  #unzip keep original
+    bzip2 -dk long_abstracts_en_uris_es.ttl.bz2  #unzip keep original
     echo "changing ttl to csv.."
-    python prep_wiki_abstracts.py -i long_abstracts_wkd_uris_es.ttl  -o dbpedia-abstracts-es.csv
+#     python prep_wiki_abstracts.py -i long_abstracts_wkd_uris_es.ttl  -o dbpedia-abstracts-es.csv
+    python prep_wiki_abstracts.py -i long_abstracts_en_uris_es.ttl  -o dbpedia-abstracts-es.csv
     rm long_abstracts_wkd_uris_es.ttl
 else 
     echo "English"
